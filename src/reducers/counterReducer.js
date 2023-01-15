2695808bd3964bf4bddabeeab968d06e6ff2e532
tushargaur3 <120725354+tushargaur3@users.noreply.github.com>
2023-01-15 23:52:55 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,11 +1,12 @@
 const counterReducer = (state,action) =>{
- switch (action.type) {
+  switch (action.type) {
    case 'INCREMENT':
      return { ...state, counter: state.counter + 1};
    case 'DECREMENT':
      return { ...state, counter: state.counter - 1};
    default:
      return state;
+  }
 }
 
 export {counterReducer}
