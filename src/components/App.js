6b14b01f62f44b7df52254d45fd8974c19be3ea6
tushargaur3 <120725354+tushargaur3@users.noreply.github.com>
2023-01-15 23:51:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React, { useReducer } from 'react'
 import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer, {counter : 0})
-const { counter } = state;
+  const [state,dispatch] = useReducer(counterReducer, {counter : 0})
+  const { counter } = state;
   return (
     <div id="main">
          <span id="counter">{counter}</span>
